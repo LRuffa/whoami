4653ba6a51c2c9cde8ff8dc9db512e0868fca2b6
Lucas Ruffa <lucasgabrielruffa@gmail.com>
2026-02-17 16:23:01 -0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ const {
 
 // https://astro.build/config
 const config = defineConfig({
-	site: 'https://spectre.lou.gg',
+	site: 'https://Rlucas.github.io',
 	output: 'static',
 	integrations: [
 		expressiveCode({
